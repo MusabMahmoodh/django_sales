3bb722e6c6192b129e0a2d21cd159f508bd79e9e
MusabMahmoodh <43021789+MusabMahmoodh@users.noreply.github.com>
2021-04-20 12:02:44 +0530

    ADD forms

--- sales/views.py
+++ sales/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Sale
+from .forms import SalesSearchForm
 # Create your views here.
 #function views are more readable than class views
 def home_view(request):
-    return render(request,'sales/home.html',{})
+    form  = SalesSearchForm(request.POST or None)
+    context = {
+        'hello':"hello world",
+        'form': form
+    }
+    return render(request,'sales/home.html',context)
 
 class SalesListView(ListView):
     #register view as a vew
