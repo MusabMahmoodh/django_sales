0556601ee54d375fa1bd827265ce820d2383927a
MusabMahmoodh <43021789+MusabMahmoodh@users.noreply.github.com>
2021-04-20 10:59:46 +0530

    initial view and template

--- profiles/signals.py
+++ profiles/signals.py
@@ -3,10 +3,12 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+# After saving the user --> create profile
 @receiver(post_save, sender=User)
 def post_save_create_profile(sender, instance, created, **kwargs):
     print(sender)
     print(instance)
     print(created)
+    # If this is the first time creation
     if created:
         Profile.objects.create(user=instance)
--- sales/views.py
+++ sales/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+#function views are more readable than class views
+def home_view(request):
+    return render(request,'sales/main.html',{})
