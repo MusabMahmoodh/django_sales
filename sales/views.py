90204a0459c5e33bb95dab4bf397aeafd6be9194
MusabMahmoodh <43021789+MusabMahmoodh@users.noreply.github.com>
2021-04-21 11:30:49 +0530

    add to form

--- sales/views.py
+++ sales/views.py
@@ -6,8 +6,12 @@ from .forms import SalesSearchForm
 #function views are more readable than class views
 def home_view(request):
     form  = SalesSearchForm(request.POST or None)
+    
+    if(request.method == "POST"):
+        date_from = request.POST.get('date_from')
+        date_to = request.POST.get('date_to')
+        chart_type = request.POST.get('chart_type')
     context = {
-        'hello':"hello world",
         'form': form
     }
     return render(request,'sales/home.html',context)
